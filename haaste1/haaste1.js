7a96d83aa173f1b423f386f551fa5cce285213fe
Elina <elina.postila@edu.keuda.fi>
2021-04-12 11:34:12 +0300

    eka lataus

---
+++ haaste1/haaste1.js
@@ -0,0 +1,32 @@
+function laskeYhteen(eka, toka) {
+  console.log(eka + toka);
+}
+
+function ekaMuuttuja() {
+  var muuttuja = [34, 566, 71, 89];
+  console.log(muuttuja[0]);
+}
+
+function alleSata(eka, toka) {
+  if (eka + toka < 100) {
+    console.log("Lukujen summa on alle 100");
+  } else {
+    console.log("Luvut on liian suuria, jotta pystyisin laskemaan niitä!");
+  }
+}
+
+function onkoSama(eka, toka) {
+  if (eka === toka) {
+    console.log("Samat!");
+  } else {
+    console.log("Eri luvut!");
+  }
+}
+
+function tunnit(h, m, s) {
+  console.log(h * 60 * 60 + m * 60 + s);
+}
+
+function painInAss() {
+  
+}
